df436e3032c622a4c6c40a7d7b74b721c0de2eaf
adiabat <adiabat@e214m08.istic.univ-rennes1.fr>
2020-01-10 14:49:28 +0100

    Update

---
+++ src/main/java/jpa/Allergies.java
@@ -0,0 +1,47 @@
+package jpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Allergies {
+
+	private Long id;
+	private String libelle;
+	private Participant participant;
+	
+	public Allergies() {
+		
+	}
+	
+	public Allergies(Long id, String libelle) {
+		this.id = id;
+		this.libelle = libelle;
+		participant = new Participant();
+	}
+	
+	@Id
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	public String getLibelle() {
+		return libelle;
+	}
+	
+	public void setParticipant(Participant participant) {
+		this.participant = participant;
+	}
+	
+	@ManyToOne
+	public Participant getParticipant() {
+		return participant;
+	}
+}
--- src/main/java/jpa/Mail.java
+++ src/main/java/jpa/Mail.java
@@ -1,7 +1,78 @@
 package jpa;
 import javax.persistence.*;
 
+@Entity
 public class Mail {
 	
+	private Participant participant;
+	private Reunion reunion;
+	private String lienPause;
+	private String code;
+	private String lienPad;
+	private Long id;
 	
+	public Mail() {
+		
+	}
+	
+	public Mail(Long id, String lienPause, String code, String lienPad) {
+		this.code = code;
+		this.lienPad = lienPad;
+		this.lienPause = lienPause;
+		this.id = id;
+		participant = new Participant();
+		reunion = new Reunion();
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setLienPad(String lienPad) {
+		this.lienPad = lienPad;
+	}
+	
+	public String getLienPad() {
+		return lienPad;
+	}
+	
+	public void setLienPause(String lienPause) {
+		this.lienPause = lienPause;
+	}
+	
+	public String getLienPause() {
+		return lienPause;
+	}
+	
+	
+	public void setParticipant(Participant participant) {
+		this.participant = participant;
+	}
+	
+	@OneToMany
+	public Participant getParticipant() {
+		return participant;
+	}
+	
+	public void setReunion(Reunion reunion) {
+		this.reunion = reunion;
+	}
+	
+	@OneToMany
+	public Reunion getReunion() {
+		return reunion;
+	}
+	
+	@Id
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
 }
