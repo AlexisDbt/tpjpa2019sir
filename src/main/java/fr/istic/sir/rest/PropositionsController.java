a4440b8fec16d52bc81dfdc95043d87947bcd492
celieRlt <celie.rault@gmail.com>
2020-05-07 16:30:38 +0200

    add a new controller

--- src/main/java/fr/istic/sir/rest/PropositionsController.java
+++ src/main/java/fr/istic/sir/rest/PropositionsController.java
@@ -8,7 +8,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.text.DateFormat;
 import java.util.List;
+import java.util.Locale;
 
 @Path("/sondage")
 public class PropositionsController {
